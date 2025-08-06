c7764f39d750117ae972e2d39d280784e7e7f814
狂想ThePotato <110756250+Kasualix@users.noreply.github.com>
2025-08-06 15:03:06 +0800

    speedup config update

--- src/main/java/me/kall/fluidium/Fluidium.java
+++ src/main/java/me/kall/fluidium/Fluidium.java
@@ -1,5 +1,6 @@
 package me.kall.fluidium;
 
+import com.google.common.base.Suppliers;
 import me.kall.fluidium.common.api.IFluid;
 import me.kall.fluidium.common.config.FluidiumConfig;
 import net.minecraft.core.BlockPos;
@@ -18,11 +19,18 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.ForgeRegistries;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
 
 @Mod(Fluidium.MOD_ID)
 public final class Fluidium {
     public static final String MOD_ID = "fluidium";
+
+    private static final Supplier<Set<ResourceLocation>> BLACKLIST = Suppliers.memoize(() -> FluidiumConfig.BLACKLIST.get().stream().map(ResourceLocation::parse).collect(Collectors.toSet()));
+    private static final Supplier<Set<String>> MOD_ID_LIST = Suppliers.memoize(() -> new HashSet<>(FluidiumConfig.MOD_ID_LIST.get()));
 
     public Fluidium(@NotNull FMLJavaModLoadingContext context) {
         context.registerConfig(ModConfig.Type.COMMON, FluidiumConfig.INSTANCE);
@@ -36,7 +44,7 @@ public final class Fluidium {
             ForgeRegistries.FLUIDS.getEntries().forEach(entry -> {
                 ResourceLocation id = entry.getKey().location();
                 Fluid fluid = entry.getValue();
-                boolean alwaysTick = FluidiumConfig.BLACKLIST.get().contains(id.toString()) || FluidiumConfig.MOD_ID_LIST.get().contains(id.getNamespace());
+                boolean alwaysTick = BLACKLIST.get().contains(id) || MOD_ID_LIST.get().contains(id.getNamespace());
                 ((IFluid) fluid).fluidium$setShouldAlwaysTick(alwaysTick);
             });
         }
